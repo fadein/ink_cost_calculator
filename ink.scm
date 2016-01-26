5c899f73312e46667fd1806cd6756d05d70b8b5f
Erik on RPi <ewfalor@gmail.com>
2016-01-25 23:13:07 -0700

    updated to use records

--- ink.scm
+++ ink.scm
@@ -1,30 +1,51 @@
-(use fmt)
+#!/usr/local/bin/csi -s
 
+(use fmt)
 
 (define *mL-per-oz* 29.5735295625) 
 (define (oz->mL oz) (* oz *mL-per-oz*))
 (define (mL->oz mL) (* mL (/ *mL-per-oz*)))
 
+(define-record bottle
+			   brand
+			   volume unit
+			   price)
 
-(define *noodlers-bottle-cost* 12.50)
-(define *noodlers-bottle-volume* (oz->mL 3))
-(define noodlers-per-mL (/ *noodlers-bottle-cost* *noodlers-bottle-volume*))
+(define (ink-price-printer ink)
+  (let ((norm-volume (if (equal? (bottle-unit ink) 'ml)
+					   (bottle-volume ink)
+					   (oz->mL (bottle-volume ink)))))
 
-(define *diamine-bottle-cost* 14.95)
-(define *diamine-bottle-volume* 80)
-(define diamine-per-mL (/ *diamine-bottle-cost* *diamine-bottle-volume*))
+	(fmt #t (bottle-brand ink) " is $" (fix 3 (/ (bottle-price ink) norm-volume)) " per mL" nl)))
 
-(define *montblanc-bottle-cost* 19.00)
-(define *montblanc-bottle-volume* 60)
-(define montblanc-per-mL (/ *montblanc-bottle-cost* *montblanc-bottle-volume*))
 
-(define *pelikan-edelstein-cost-sale* (* .85 25.20))
-(define *pelikan-edelstein-cost* 28)
-(define *pelikan-edelstein-volume* 50)
-(define edelstein-per-mL (/ *pelikan-edelstein-cost-sale* *pelikan-edelstein-volume*))
+(define noodlers (make-bottle
+				   "Noodler's"
+				   3 'oz
+				   12.50))
 
-(fmt #t "Noodler's is $" (fix 3 noodlers-per-mL) " per mL" nl)
-(fmt #t "Diamine   is $" (fix 3 diamine-per-mL ) " per mL" nl)
-(fmt #t "Montblanc is $" (fix 3 montblanc-per-mL) " per mL" nl)
-(fmt #t "Edelstein is $" (fix 3 (/ *pelikan-edelstein-cost-sale* *pelikan-edelstein-volume*)) " per mL on sale" nl)
-(fmt #t "Edelstein is $" (fix 3 (/ *pelikan-edelstein-cost* *pelikan-edelstein-volume*)) " per mL regular" nl)
+(define diamine (make-bottle
+				  "Diamine"
+				  80 'ml
+				  14.95))
+
+(define montblanc (make-bottle
+					"Montblanc"
+					60 'ml
+					19.00))
+
+(define edelstein (make-bottle
+					"Pelikan Edelstein"
+					50 'ml
+					28.00))
+
+(define edelstein-sale (make-bottle
+					"Pelikan Edelstein (on sale)"
+					50 'ml
+					(* .85 25.20)))
+
+(ink-price-printer noodlers)
+(ink-price-printer diamine)
+(ink-price-printer montblanc)
+(ink-price-printer edelstein)
+(ink-price-printer edelstein-sale)
